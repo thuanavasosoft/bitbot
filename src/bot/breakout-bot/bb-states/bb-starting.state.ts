562e02be70464bc00fdeecf3f6acc1ea9726fc35
Thuan Nguyen <thuan.nguyen@avasosoft.com>
2025-12-22 06:57:58 +0000

    (feat): Don't set margin mode

--- src/bot/breakout-bot/bb-states/bb-starting.state.ts
+++ src/bot/breakout-bot/bb-states/bb-starting.state.ts
@@ -44,24 +44,6 @@ class BBStartingState implements BBState {
     TelegramService.queueMsg(msg1)
   }
 
-  private async _ensureIsolatedMarginMode() {
-    const msg = `Setting margin mode of ${this.bot.symbol} to Isolated...`;
-    console.log(msg);
-    TelegramService.queueMsg(msg);
-
-    const success = await ExchangeService.setMarginMode(this.bot.symbol, "isolated");
-    if (!success) {
-      const errMsg = `Failed to set margin mode of ${this.bot.symbol} to Isolated`;
-      console.error(errMsg);
-      TelegramService.queueMsg(`❌ ${errMsg}`);
-      throw new Error(errMsg);
-    }
-
-    const successMsg = `Margin mode set to Isolated successfully`;
-    console.log(successMsg);
-    TelegramService.queueMsg(successMsg);
-  }
-
   private async _updatePricePrecision() {
     const symbolInfo = await ExchangeService.getSymbolInfo(this.bot.symbol);
     this.bot.symbolInfo = symbolInfo;
@@ -81,12 +63,10 @@ class BBStartingState implements BBState {
       (!this.bot.startQuoteBalance) && this._updateBotStartBalances(),
       (!this.bot.currQuoteBalance) && this.updateBotCurrentBalances(),
       this._updateLeverage(),
-      this._ensureIsolatedMarginMode(),
       this.bot.loadSymbolInfo(),
       this._updatePricePrecision(),
     ]);
 
-    console.log('Breakout Bot MEXC Entering Starting State')
     TelegramService.queueMsg("⚙️ Starting Bot");
 
     const msg = `
