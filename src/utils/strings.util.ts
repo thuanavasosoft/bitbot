cce3377cc0192d56f15319d212f95b9f4bbe1198
fatwaanugerah0421 <fatwaanugerah0421@gmail.com>
2026-09-10 11:07:02 +0800

    Add maintanance margin in the new opened position message

--- src/utils/strings.util.ts
+++ src/utils/strings.util.ts
@@ -50,6 +50,7 @@ Side: ${position.side}
 Leverage: X${position.leverage}
 Size: ${position.size}
 Notional Value: ${position.notional}
+Maintanance Margin: ${position.maintenanceMargin}
 
 Liquidation Price: ${position.liquidationPrice}
 Avg Price: ${position.avgPrice}
